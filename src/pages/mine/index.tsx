482d913e480275182eeb248a743a380cb468c063
Avril <1259150970@qq.com>
2022-12-14 13:24:57 +0800

    feat:个人中心100%

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -3,6 +3,8 @@ import default_avatar from "@/assets/img/avatar.png";
 import doctor from "@/assets/img/doctor.png";
 import { useEffect, useState } from "react";
 import { UserInfo } from "@/services/types";
+import register from "@/assets/img/register.png";
+import ask from "@/assets/img/ask.png";
 import "./index.scss";
 
 export default function Mine() {
@@ -21,6 +23,7 @@ export default function Mine() {
 
   return (
     <View className='index'>
+
       <View className='inf-box'>
         {/* 头像&&用户名&&账号 */}
         <View className='inf-detail-box'>
@@ -48,7 +51,36 @@ export default function Mine() {
         }
         <Image src={doctor} className='img' />
       </View>
+
       <View className='gray-line'></View>
+  
+      <View className='func-box'>
+        {/* 我的收藏&&就诊人管理 */}
+        <View className='func-line-box'>
+          <View className='func-line-little-box'>
+            <Text className='func-num-font'>27</Text>
+            <Text className='func-name-font'>我的收藏</Text>
+          </View>
+          <View className='fuc-gray-line'></View>
+          <View className='func-line-little-box'>
+            <Text className='func-num-font'>3</Text>
+            <Text className='func-name-font'>就诊人管理</Text>
+          </View>
+        </View>
+        {/* 订单管理 */}
+        <View className='func-color-box'>
+          <View className='func-color-little-box'>
+            <Text className='func-color-font'>挂号记录</Text>
+            <Image src={register} className='func-color-img register-img' />
+          </View>
+          <View className='func-color-little-box'>
+            <Text className='func-color-font'>问诊记录</Text>
+            <Image src={ask} className='func-color-img ask-img' />
+          </View>
+        </View>
+        {/* 退出登录按钮 */}
+        <View className='log-out-btn'>退出登录</View>
+      </View>
     </View>
   );
 }
